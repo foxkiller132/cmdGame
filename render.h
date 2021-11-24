412e0459e9e596ac4a946c127e40f8ddcd12c558
Chase Ward <88118323+foxkiller132@users.noreply.github.com>
2021-11-23 19:17:33 -0600

    fix array size

--- render.h
+++ render.h
@@ -7,7 +7,7 @@ private:
 public:
     Render();
     ~Render();
-    void draw(char map[50][50]);
+    void draw(char screen[100][50]);
     void setCursorPosition(int x, int y);
     char updateScreen(char **map);
 };
